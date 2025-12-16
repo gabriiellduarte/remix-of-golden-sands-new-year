e383afaf5eee2f67ec623c0903a03c2318c6d04b
Gabriel Duarte <gabriel.duarte@aracati.ce.gov.br>
2025-12-16 11:47:47 -0300

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["canoa2026.aracati.ce.gov.br"]
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
